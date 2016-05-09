686280320bcd0c1af5642daa34380b490244c82d
Joey <joeyarnstein@gmail.com>
2016-05-09 09:55:15 -0700

    passing specs for save method to test database

--- src/main/java/spark/Stylist.java
+++ src/main/java/spark/Stylist.java
@@ -24,26 +24,26 @@ public class Stylist{
     }
   }
 
-  // @Override
-  // public boolean equals(Object otherStylist) {
-  //   if (!(otherStylist instanceof Stylist)) {
-  //     return false;
-  //   } else {
-  //     Stylist newStylist = (Stylist) otherStylist;
-  //     return this.getName().equals(newStylist.getName()) &&
-  //            this.getId() == newStylist.getId();
-  //   }
-  // }
-  //
-  // public void save() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String sql = "INSERT INTO stylists(name) VALUES (:name)";
-  //     this.id = (int) con.createQuery(sql, true)
-  //       .addParameter("name", this.name)
-  //       .executeUpdate()
-  //       .getKey();
-  //   }
-  // }
+  @Override
+  public boolean equals(Object otherStylist) {
+    if (!(otherStylist instanceof Stylist)) {
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getName().equals(newStylist.getName()) &&
+             this.getId() == newStylist.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
   //
   // public static Stylist find(int id) {
   //   try(Connection con = DB.sql2o.open()) {
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -24,4 +24,17 @@ public class StylistTest {
   public void all_emptyAtFirst() {
     assertEquals(Stylist.all().size(), 0);
   }
+  @Test
+  public void save_savesIntoDatabase_true() {
+    Stylist test = new Stylist("Stacy");
+    test.save();
+    assertTrue(Stylist.all().get(0).equals(test));
+  }
+  @Test
+  public void save_assignsIdToObject() {
+    Stylist myStylist = new Stylist("Household chores");
+    myStylist.save();
+    Stylist savedStylist = Stylist.all().get(0);
+    assertEquals(myStylist.getId(), savedStylist.getId());
+  }
 }
